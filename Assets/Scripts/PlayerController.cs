be9b55c06662826af2a3eae53c393e8e8a32db6d
Naziya <ahmedi.n@indivillage.in>
2025-10-23 16:48:49 +0530

    Setting the player position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
-      horizontalInput = Input.GetAxis(“Horizontal”);  
+        if (transform.postion.x < -10) {
+            transform.postion = new Vector3(-10, transform.postion.y, transform.postion.z);
+
+        }
+        
+      horizontalInput = Input.GetAxis("Horizontal");  
+      transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
