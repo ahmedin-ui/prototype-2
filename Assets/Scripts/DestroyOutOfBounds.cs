885b0721a5026535e470d7295138600ec68e54e4
Naziya <ahmedi.n@indivillage.in>
2025-10-24 12:41:39 +0530

    Destroy the gameobject if it is beyound the given range

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
+    private float topBound = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
