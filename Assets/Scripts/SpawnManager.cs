34d5ecca1ced0ad189287bf0455fa525023186a9
Naziya <ahmedi.n@indivillage.in>
2025-10-24 16:31:52 +0530

    spawn the animals on a timer

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,6 +11,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
         
     }
 
