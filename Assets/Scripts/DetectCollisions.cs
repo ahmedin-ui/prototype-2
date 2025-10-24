97bc4b358c7070ce70a69c5808aa681b2d501230
Naziya <ahmedi.n@indivillage.in>
2025-10-24 17:37:43 +0530

    Trigger a “Game Over” message

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -18,7 +18,6 @@ public class DetectCollisions : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-         
+        Destroy(gameObject);    
     }
 }
